6305c247b770e74fdc0339a89bb0d393d6aec009
raulereno <raulereno@gmail.com>
2022-03-15 10:16:18 -0300

    commit de seguridad 15/3/22

--- src/componentes/homePage/HomePage.jsx
+++ src/componentes/homePage/HomePage.jsx
@@ -8,7 +8,7 @@ function Inicio() {
 
         <h1 className='defIng'>Non-Fungible Token</h1>
         <h2 className='defEs'>Token No Fungible</h2>
-        <p className='explicacion'>Un token no fungible (en inglés, non-fungible token), o NFT por sus siglas en inglés, es un tipo especial de token criptográfico que representa algo único. Los tókenes no fungibles no son, por tanto, mutuamente intercambiables.23 Esto contrasta con las criptomonedas como el bitcoin, y muchos tókenes de red o de utilidad que son fungibles por naturaleza.4 Las cuatro principales características de los NFT es que son únicos, indivisibles, transferibles y con la capacidad de demostrar su escasez.</p>
+        <p className='explicacion'>Un token no fungible (en inglés, non-fungible token), o NFT por sus siglas en inglés, es un tipo especial de token criptográfico que representa algo único. Los tókenes no fungibles no son, por tanto, mutuamente intercambiables. Esto contrasta con las criptomonedas como el bitcoin, y muchos tókenes de red o de utilidad que son fungibles por naturaleza. Las cuatro principales características de los NFT es que son únicos, indivisibles, transferibles y con la capacidad de demostrar su escasez.</p>
         <p className='explicacion'>
         Los NFT también han sido objeto de críticas, principalmente respecto a su alto coste energético y la huella de carbono asociada a la validación de transacciones en la cadena de bloques, así como por su frecuente uso en estafas virtuales. Otras críticas son referidas a la verdadera utilidad de establecer un comprobante de propiedad en un, a veces extralegal, mercado no regulado. 
         </p>
